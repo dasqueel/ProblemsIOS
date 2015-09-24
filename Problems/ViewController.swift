d1da04b0f15baaa2bc2d11d9dfa6c6050dedf204
Neil Barduson <barduson.neil@gmail.com>
2015-09-24 18:58:08 -0500

    Initial Commit

---
+++ Problems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Problems
+//
+//  Created by Neil Thomas Barduson on 9/24/15.
+//  Copyright (c) 2015 Problems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
